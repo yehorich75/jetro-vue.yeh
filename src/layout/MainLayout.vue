a22f449aaa12c64fc499d9b2ce371b4d4c74b223
Yegorich75 <yegorich75@gmail.com>
2018-05-09 06:36:49 +0300

    First commit

---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>I am header</h1>
+  <slot></slot>
+  <h1>I am footer</h1>
+</template>
+
+<script>
+export default {
+  name: 'Layout'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/SinglePost.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1 class="singlepost">SinglePostPage</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SinglePost',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1.singlepost {
+  background: red;
+}
+</style>
