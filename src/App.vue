7b023ef7e5dda446f953b3fc76ef9eb8ff8dc03d
Yegorich75 <yegorich75@gmail.com>
2018-05-09 09:49:07 +0300

    Sass Syntax Complite

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ export default {
 }
 </script>
 
-<style scoped lang="sass">
+<style lang="sass">
 #app
   font-family: 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing: antialiased
@@ -22,8 +22,8 @@ export default {
   color: #2c3e50
   margin-top: 60px
 
-/* Анимации появления и исчезновения могут иметь */
-/* различные продолжительности и динамику.       */
+//  Анимации появления и исчезновения могут иметь
+//  различные продолжительности и динамику.      
 .slide-fade-enter-active 
   transition: all .3s ease
 
@@ -31,7 +31,7 @@ export default {
   transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)
 
 .slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active до версии 2.1.8 */ 
+// .slide-fade-leave-active до версии 2.1.8 
   transform: translateX(10px)
   opacity: 0
   
